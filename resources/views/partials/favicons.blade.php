6c90c84b3bd56fd50c233eeed0b06d13fafb5ee3
Yves Engetschwiler <yves@interactive.swiss>
2025-10-01 14:06:50 +0200

    Added favicons

--- resources/views/partials/favicons.blade.php
+++ resources/views/partials/favicons.blade.php
@@ -1,7 +1,6 @@
-<link rel="icon" type="image/png" href="/favicon-96x96.png?v=923048" sizes="96x96" />
-<link rel="icon" type="image/svg+xml" href="/favicon.svg?v=923048" />
-<link rel="shortcut icon" href="/favicon.ico?v=923048" />
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=923048" />
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 <link rel="manifest" href="/site.webmanifest" />
-<link rel="mask-icon" href="/safari-pinned-tab.svg?v=923048" color="#142e46">
-<meta name="theme-color" content="#142e46">
+<meta name="theme-color" content="#f4f4f5">
